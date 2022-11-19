21dcef775f9f6b1460e5437f8b636b380a461b52
wali55 <walisantunu@gmail.com>
2022-11-19 11:30:38 +0600

    cart design

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -5,6 +5,8 @@ const Cart = () => {
     return (
         <div className="cart">
             <h2>Selected Items</h2>
+            <button className="chose-one-btn">Chose One for Me</button>
+            <button className="chose-again-btn">Chose Again</button>
         </div>
     );
 };
